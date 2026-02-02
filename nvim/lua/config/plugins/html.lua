1126dc4053dddd1b5684d79c344bd0be87d3bae3
dallenb4 <dallenb4@proton.me>
2026-02-02 12:12:23 -0700

    HTML live reload plugin

--- nvim/lua/config/plugins/html.lua
+++ nvim/lua/config/plugins/html.lua
@@ -2,5 +2,13 @@ return {
 	{
 		"windwp/nvim-ts-autotag",
 		opts = {}
+	},
+	{
+		"brianhuster/live-preview.nvim",
+		opts = {
+			browser = nil,
+			address = '0.0.0.0',
+			port = 3000
+		}
 	}
 }
